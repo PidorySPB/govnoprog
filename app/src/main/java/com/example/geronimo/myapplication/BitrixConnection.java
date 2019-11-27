bdf3d7240a5497aeb8b603d832306782d379e99d
Geronimo <artem-artem-68@bk.ru>
2019-11-27 20:01:54 +0300

    New project

---
+++ app/src/main/java/com/example/geronimo/myapplication/BitrixConnection.java
@@ -0,0 +1,90 @@
+package com.example.geronimo.myapplication;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.net.ssl.HttpsURLConnection;
+
+import okhttp3.ConnectionSpec;
+import okhttp3.FormBody;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class BitrixConnection extends AsyncTask<String,Void,String> {
+    public static final String REQUEST_METHOD = "POST";
+    public static final int READ_TIMEOUT = 15000;
+    public static final int CONNECTION_TIMEOUT = 15000;
+
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
+
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+        String stringUrl = strings[0];
+        String result="";
+        String inputLine;
+        try {
+            //Create a URL object holding our url
+            URL myUrl = new URL(stringUrl);
+            //Create a connection
+            HttpURLConnection connection =(HttpURLConnection)
+                    myUrl.openConnection();
+            //Set methods and timeouts
+            connection.setRequestMethod(REQUEST_METHOD);
+            connection.setReadTimeout(READ_TIMEOUT);
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+            //Connect to our url
+            connection.connect();
+            //Create a new InputStreamReader
+            InputStreamReader streamReader = new
+                    InputStreamReader(connection.getInputStream());
+            //Create a new buffered reader and String Builder
+            BufferedReader reader = new BufferedReader(streamReader);
+            StringBuilder stringBuilder = new StringBuilder();
+            //Check if the line we are reading is not null
+            while((inputLine = reader.readLine()) != null){
+                stringBuilder.append(inputLine);
+            }
+            //Close our InputStream and Buffered reader
+            reader.close();
+            streamReader.close();
+            //Set our result equal to our stringBuilder
+            result = stringBuilder.toString();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
---
+++ app/src/main/java/com/example/geronimo/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.example.geronimo.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+//BitrixConnection connection= new BitrixConnection();
+BitrixConnection bc;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void LoginBtn(View v) {
+        String password=((EditText)findViewById(R.id.editTextPassword)).getText().toString();
+        String login=((EditText)findViewById(R.id.editTextLogin)).getText().toString();
+        if (login == "") {
+//вывод ошибки
+        } else if (password == "") {
+            //вывод ошибки
+        } else {
+            try {
+                //connection.login(login, password, "http://cc25455-bitrix-1.tw1.ru/mobile/Auth.php");
+
+                bc.execute("http://cc25455-bitrix-1.tw1.ru/mobile/Auth.php",login,password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
